bd777caa68408a03d3f03d542211ebdd575f51c5
aureliafriska <aurelia_friska@yahoo.com>
2017-04-20 12:48:19 +0700

    Delete folder

---
+++ app/src/main/java/com/aureliafriska/MainActivity.java
@@ -0,0 +1,52 @@
+package com.aureliafriska.androidbottomnavigationview;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private BottomNavigationView bottomNavigation;
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        bottomNavigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigation.inflateMenu(R.menu.bottom_navigation_items);
+        fragmentManager = getSupportFragmentManager();
+        
+        //Untuk inisialisasi fragment pertama kali
+        fragmentManager.beginTransaction().replace(R.id.main_container, new FragmentBuku()).commit();
+        
+        //Memberikan listener saat menu item di bottom navigation diklik
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.menu_buku:
+                        fragment = new FragmentBuku();
+                        break;
+                    case R.id.menu_musik:
+                        fragment = new FragmentMusik();
+                        break;
+                    case R.id.menu_film:
+                        fragment = new FragmentFilm();
+                        break;
+                }
+                final FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.main_container, fragment).commit();
+                return true;
+            }
+        });
+    }
+}
